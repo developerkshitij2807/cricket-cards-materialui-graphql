cef9484a7e590012ff933104f691330a3413fa74
Kshitij Dutt <kshtjdutt07@gmail.com>
2023-02-28 19:59:50 +0530

    added url in apollo-client

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -6,7 +6,11 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: ApolloLink.from([new HttpLink({ uri: "/api/graphql" })]),
+  link: ApolloLink.from([
+    new HttpLink({
+      uri: "https://cricket-cards-materialui-graphql-developerkshitij2807.vercel.app/",
+    }),
+  ]),
   cache: new InMemoryCache(),
 });
 
